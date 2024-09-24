18f800d5bc3812748fe75a300aa2c7417e392c52
Admin <reteamboss@gmail.com>
2024-09-24 17:01:37 +0300

    1

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,18 @@
 fun main() {
-    println("Hello World!")
+
+    val b = ToBin(10)
+    MyNum(b).toBin()
+
 }
+
+interface Binary{
+    fun toBin()
+}
+
+class ToBin(val num: Int): Binary{
+    override fun toBin() {
+        println("Число в бинарной системе счисления ${Integer.toBinaryString(num)}")
+    }
+}
+
+class MyNum(num: Binary): Binary by num
